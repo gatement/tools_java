26a37cd1a7264f5772d4af2da3df8cf584347c57
liugh22 <guanhua.liu@midea.com>
2019-07-03 10:59:25 +0800

    go

--- src/test/java/johnson/tools/encryption/asymmetry/RSAUtilTest.java
+++ src/test/java/johnson/tools/encryption/asymmetry/RSAUtilTest.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import org.apache.commons.codec.binary.Base64;
 import org.junit.Test;
 
+import johnson.tools.Util;
+
 public class RSAUtilTest {
 
 	@Test
@@ -18,8 +20,8 @@ public class RSAUtilTest {
 		Map<String, Object> keyMap = RSAUtil.initKey();
 		byte[] privateKey = RSAUtil.getPrivatekey(keyMap);
 		byte[] publicKey = RSAUtil.getPublickey(keyMap);
-		System.out.println("private key = " + Base64.encodeBase64String(privateKey));
-		System.out.println("public key = " + Base64.encodeBase64String(publicKey));
+		//System.out.println("private key = " + Base64.encodeBase64String(privateKey));
+		//System.out.println("public key = " + Base64.encodeBase64String(publicKey));
 		
 		byte[] privateKeyEncrypted = RSAUtil.encryptByPrivateKey(inputData, privateKey);
 		byte[] publicKeyDecrypted = RSAUtil.decryptByPublicKey(privateKeyEncrypted, publicKey);
@@ -33,7 +35,9 @@ public class RSAUtilTest {
 	@Test
 	public void testSignature() throws Exception {
 		String inputStr = "Johnson";
+		System.out.println("text to signature = " + inputStr);
 		byte[] inputData = inputStr.getBytes("UTF-8");
+		Util.printBytes(inputData);
 		
 		Map<String, Object> keyMap = RSAUtil.initKey();
 		byte[] privateKey = RSAUtil.getPrivatekey(keyMap);
@@ -41,6 +45,7 @@ public class RSAUtilTest {
 
 		byte[] sign = RSAUtil.sign(inputData, privateKey);
 		System.out.println("RSA signature = " + Base64.encodeBase64String(sign));
+		Util.printBytes(sign);
 		
 		boolean verification = RSAUtil.verify(inputData, publicKey, sign);
 		assertTrue(verification);
